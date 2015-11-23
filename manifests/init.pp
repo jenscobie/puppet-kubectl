345c362aa72efe839fb9c8c406b29ede7251fc2e
jenscobie <jenscobie@gmail.com>
2015-11-22 17:25:35 -0800

    Remove dependency on wget

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,17 +7,13 @@
 
 class kubectl {
 
-  include wget
-
-  $download_url = 'https://storage.googleapis.com/kubernetes-release/release/v1.0.1/bin/darwin/amd64/kubectl'
+  $download_uri = 'https://storage.googleapis.com/kubernetes-release/release/v1.0.1/bin/darwin/amd64/kubectl'
 
   exec { 'download kubectl':
-    command => "wget -q --timestamping -P ${boxen::config::bindir} $download_url",
+    command => "curl -s -L ${download_uri} -o ${boxen::config::bindir}/kubectl",
     creates => "${boxen::config::bindir}/kubectl",
-  }
-
+  } ->
   file { "${boxen::config::bindir}/kubectl":
     mode   => '0111',
-    require => Exec['download kubectl']
   }
 }
--- spec/classes/kubectl_spec.rb
+++ spec/classes/kubectl_spec.rb
@@ -4,10 +4,6 @@ describe 'kubectl' do
   let(:facts) { default_test_facts }
 
   it do
-    should include_class('wget')
-
-    should have_exec_resource_count(1)
-
     should contain_file('/test/boxen/bin/kubectl').with({
       'mode'   => '0111',
     })
